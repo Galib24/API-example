eaec17d84ec5c8a5cde48b1e7365fc0ee8c05717
SQLAdmin <abuyeahia24@gmail.com>
2023-02-24 11:44:49 +0600

    country added with api

--- js/countries.js
+++ js/countries.js
@@ -13,17 +13,42 @@ const displayCountries = countries =>{
 
 // using forEach
 countries.forEach(country => {
-    console.log(country.name.common)
+    // console.log(country.cca2)
     const countryDiv = document.createElement('div');
     countryDiv.classList.add('country')
     countryDiv.innerHTML = `
     
     <h3>Name: ${country.name.common} </h3>
     <p>capital: ${country.capital ? country.capital[0] : 'No Capital'}</p>
+    <button onclick="loadCountryDetails('${country.cca2}')">Details</button>
+    
+    
     `;
 
     countiesContainer.appendChild(countryDiv);
     
 });
+}
+
+
+const loadCountryDetails= code =>{
+    // https://restcountries.com/v3.1/alpha/{code}
+    const url = `https://restcountries.com/v3.1/alpha/${code}`
+    // console.log(url)
+    fetch(url)
+    .then(res => res.json())
+    .then(data => showCountryDetails(data[0]))
+}
+const showCountryDetails = country =>{
+    const detailContainer = document.getElementById('country-detail')
+    detailContainer.innerHTML = `
+    <h2>country Details</h2>
+       <h3>Name: ${country.name.common} </h3>
+       <p>Capital: ${country.capital}</p>
+       <img src="${country.flags.png}"> 
+    
+    
+    
+    `
 }
 loadCountries()
