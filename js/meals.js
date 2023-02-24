f03430a21fe0534c5c048782f82b775e934d9046
SQLAdmin <abuyeahia24@gmail.com>
2023-02-24 16:21:23 +0600

    eating menu added by api

--- js/meals.js
+++ js/meals.js
@@ -1,5 +1,7 @@
-const loadMeals = () => {
-    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=fish')
+const loadMeals = (searchText) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+  console.log(url)  
+  fetch(url)
         .then(res => res.json())
         .then(data => displayMeals(data.meals))
 
@@ -9,7 +11,7 @@ const displayMeals = meals => {
     // step:1 container element
     const mealContainer = document.getElementById('meals-container');
     meals.forEach(meal => {
-        console.log(meal)
+        // console.log(meal)
         // step:2 create child for each element
         const mealDiv = document.createElement('div');
         mealDiv.classList.add('col');
@@ -17,9 +19,9 @@ const displayMeals = meals => {
         mealDiv.innerHTML = `
         
         <div class="card h-100">
-        <img src="..." class="card-img-top" alt="...">
+        <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
+          <h5 class="card-title">${meal.strMeal}</h5>
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
         </div>
       </div>
@@ -29,6 +31,13 @@ const displayMeals = meals => {
         // step:4 append child
         mealContainer.appendChild(mealDiv)
 
-    });
+    });   
 }
-loadMeals()
+const searchMeal = () =>{
+const searchText = document.getElementById('search-field').value;
+// search meals
+console.log(searchText);
+loadMeals(searchText)
+}
+
+loadMeals('rice')
