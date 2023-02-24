9768ece7f987390a3a3174185e06f35f8046bdcb
SQLAdmin <abuyeahia24@gmail.com>
2023-02-24 18:06:10 +0600

    try catch and await added

--- js/quotes.js
+++ js/quotes.js
@@ -2,7 +2,30 @@ const loadQuote = () =>{
     fetch('https://api.kanye.rest/')
     .then(res => res.json())
     .then(data => displayQuote(data))
+    // .catch(error => console.log(error))
 }
+
+// // await
+// const loadQuote2 = async() =>{
+// const res = await fetch('https://api.kanye.rest/');
+//    const data = await res.json();
+//    displayQuote(data);
+// }
+
+// //    try catch
+// const loadQuote3 = async() =>{
+//  try{
+//     const res = await fetch('https://api.kanye.rest/');
+//     const data = await res.json();
+//     displayQuote(data);
+//  }
+// catch(error){
+//     console.log(error)
+// }
+// }
+
+
+
 const displayQuote = quote =>{
     // console.log(quote);
     const blockQuote = document.getElementById('quote');
